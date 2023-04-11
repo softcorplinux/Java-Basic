db317f3c954e988d631a7d1d8c73b30080ea7758
isaikin.viktor <isaikin.viktor@otr.ru>
2023-04-11 19:26:16 +0600

    02. Variables

---
+++ DataTypes/src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Victor Isaikin
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Variables/src/Main.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Victor Isaikin
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        int age = 40;
+        System.out.println(age);
+
+        String name = "Victor";
+        System.out.println(name);
+
+        String lastName = "Isaikin";
+        
+    }
+}
