0dbca33a0f0429eab1c4e06def37f0bfddb89e4d
isaikin.viktor <isaikin.viktor@otr.ru>
2023-04-11 14:19:17 +0600

    init

---
+++ HelloWorld/src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Victor Isaikin
+ */
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Hello World");
+        System.out.println("Hello World 2");
+    }
+}
