53734c048e6773221f741cce47e99311d6c72466
isaikin.viktor <isaikin.viktor@otr.ru>
2023-04-12 00:10:33 +0600

    03. dataTypes

--- DataTypes/src/Main.java
+++ DataTypes/src/Main.java
@@ -4,6 +4,33 @@
 
 public class Main {
     public static void main(String[] args) {
+        byte b = 10;
+        System.out.println(b);
+        System.out.println(Byte.MAX_VALUE);
+
+        short s = -16000;
+        System.out.println(s);
+
+        int i = 343354543;
+        System.out.println(i);
+        System.out.println(Integer.MAX_VALUE);
+
+        long l = 2147483648l;
+        System.out.println(l);
+
+        float f = 3.14f;
+        System.out.println(f);
+
+        double d = 345345.343;
+        System.out.println(d);
+
+        char c = 'a';
+        System.out.println(c);
+        System.out.println(++c);
+
+        boolean bool = 5 > 2;
+        System.out.println(bool);
+
 
     }
 }
