8637145bd66aa163c027ffd53bb9149557561339
isaikin.viktor <isaikin.viktor@otr.ru>
2023-04-21 15:32:47 +0600

    10. for

---
+++ 10. for/for/src/Main.java
@@ -0,0 +1,22 @@
+/**
+ * Created by Victor Isaikin
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        for(int index = 0; index < 4; index++){
+            System.out.println("Book " + index);
+        }
+
+//        int index = 0;
+//        for( ; index < 4; ){
+//            System.out.println("Book " + index);
+//            index++;
+//        }
+
+//        int num = 100;
+//        for(int index = 0, age = 40; index < 4 && age > 20; index++, age -= 10, num += 20){
+//            System.out.println("Book " + index + " Age: " + age + " Num: " + num);
+//        }
+    }
+}
