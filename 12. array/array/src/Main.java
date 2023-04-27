e765520f80a56934a6c5b15d4862ae200bd8866f
isaikin.viktor <isaikin.viktor@otr.ru>
2023-04-27 09:10:11 +0600

    12. array

---
+++ 12. array/array/src/Main.java
@@ -0,0 +1,22 @@
+/**
+ * Created by Victor Isaikin
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {100,200,300,400,500};
+
+        for(int index = 0; index < array.length; index++){
+            System.out.println("Index = " + index + " Value = " + array[index]);
+        }
+
+//        String[] users = {"Victor", "Denis", "Natalia", "Lisa"};
+//
+//        for (int index = 0; index < users.length; index++){
+//            if(index % 2 == 0){
+//                continue;
+//            }
+//            System.out.println(users[index]);
+//        }
+    }
+}
